eab092b4142e03c4961cb996be9f68dcb0491131
SaryelNarvaezPerez <94199031+SaryelNarvaezPerez@users.noreply.github.com>
2023-07-10 14:38:43 -0500

    feature: method for register and search of users

--- braille.js
+++ braille.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const braille = require('braille');
+const fs = require('fs');
 var router = express.Router();
 
+
+router.get('/welcome', (req, res) => {
+     res.send('hola mundo');
+})
 
 router.post('/hasConvert', (req, res) => {
      const { text } = req.body;
@@ -17,6 +22,48 @@ router.post('/hasConvert', (req, res) => {
           });
      }
 });
+
+
+//Registra los usuarios en el json
+router.post('/register', (req, res) => {
+     const { name, lastname, nickname, email, password } = req.body;
+     const nuevoUsuario = { name, lastname, nickname, email, password };
+
+     let usuarios = [];
+     try {
+          const data = fs.readFileSync('users.json');
+          usuarios = JSON.parse(data);
+          usuarios.push(nuevoUsuario);
+          fs.writeFileSync('users.json', JSON.stringify(usuarios));
+          res.send('¡Registro exitoso!');
+     } catch (error) {
+          console.log(error.message);
+     }
+})
+
+//Busqueda de usuario en json
+router.post('/searchUsers', (req, res) => {
+     const { email, password } = req.body;
+     let usuarios = [];
+     try {
+          const data = fs.readFileSync('users.json');
+          let usuarios = JSON.parse(data);
+          const userByEmailPass = usuarios.find(usuario => usuario.email === email && usuario.password === password);
+          console.log(userByEmailPass);
+          if (userByEmailPass != undefined) {
+               return res.status(200).json({
+                    message: 'Usuario Encontrado',
+                    braille: userByEmailPass,
+               });
+          }else {
+               return res.status(200).json({
+                    message: 'Datos invalidos'
+               });
+          }
+     } catch (error) {
+          console.log(error.message);
+     }
+})
 
 
 
